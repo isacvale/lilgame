d3f859e0223026fbcab689cf6b97dd3c54b419b3
isacvale <isac@ocastudios.com>
2022-01-02 01:48:55 -0800

    Create basic components.

---
+++ scripts/utils.js
@@ -0,0 +1,22 @@
+export const debounce = (func, delay = 100) => {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func(...args), delay;
+    });
+  };
+};
+
+export const throttle = (func, delay = 100) => {
+  let allowed = true;
+  return (...args) => {
+    if (allowed) {
+      func(...args);
+      allowed = false;
+      setTimeout(() => (allowed = true), delay);
+    } else {
+      console.log("blocked");
+    }
+  };
+};
